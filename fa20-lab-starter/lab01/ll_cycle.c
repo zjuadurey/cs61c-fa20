302debe78f8e90ba1c8f31fcaed61afa35d1862e
adurey_laptop_lenove_yoga_14c <1721966349@qq.com>
2024-04-09 22:17:45 +0800

    lab01 done

--- fa20-lab-starter/lab01/ll_cycle.c
+++ fa20-lab-starter/lab01/ll_cycle.c
@@ -3,5 +3,21 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+
+    if (!head) return 0;
+
+    node *tortoise = head, *hare = head;
+
+    while (1) {
+        if (tortoise->next && tortoise->next->next) {
+            tortoise = tortoise->next->next;
+        } else break;
+        hare = hare->next;
+
+        if (hare == tortoise) {
+            return 1;
+        } else continue;
+    }
+
     return 0;
 }
