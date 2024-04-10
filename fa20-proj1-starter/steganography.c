ebf3ec9b5a9551845ae5e13fdd97c9f6435a0e37
adurey_laptop_lenove_yoga_14c <1721966349@qq.com>
2024-04-10 19:10:49 +0800

    A2 done

--- fa20-proj1-starter/steganography.c
+++ fa20-proj1-starter/steganography.c
@@ -22,12 +22,33 @@
 Color *evaluateOnePixel(Image *image, int row, int col)
 {
 	//YOUR CODE HERE
+	Color *output = (Color *) malloc(sizeof(Color));
+	uint8_t color_val = image->image[row][col].B % 2 == 0 ? 0 : 255;
+	output->R = output->G = output->B = color_val;
+	return output;
 }
 
 //Given an image, creates a new image extracting the LSB of the B channel.
 Image *steganography(Image *image)
 {
 	//YOUR CODE HERE
+	Image *output = (Image *) malloc(sizeof(Image));
+	Color **output_image = (Color **) malloc(sizeof(Color *) * image->rows);
+
+	for (int i = 0; i < image->rows; i ++ ) {
+		output_image[i] = (Color *) malloc(sizeof(Color) * image->cols);
+		for (int j = 0; j < image->cols; j ++) {
+			Color *tmp = evaluateOnePixel(image, i, j);
+			output_image[i][j] = *tmp;
+			free(tmp);
+		}
+	}
+
+	output->cols = image->cols;
+	output->rows = image->rows;
+	output->image = output_image;
+
+	return output;
 }
 
 /*
@@ -46,4 +67,42 @@ Make sure to free all memory before returning!
 int main(int argc, char **argv)
 {
 	//YOUR CODE HERE
+	if (argc != 2) {
+		printf("usage: %s filename\n",argv[0]);
+		printf("filename is an ASCII PPM file (type P3) with maximum value 255.\n");
+		exit(-1);
+	}
+
+	//read the input file
+	Image *img = (Image*) malloc(sizeof(Image));
+	if (img == NULL) {
+		printf("a malloc fails\n.\n");
+		exit(-1);
+	}
+	char Type[10];
+	int color_stage;
+	FILE *fp = fopen(argv[1], "r");
+	fscanf(fp, "%s %u %u %d", Type, &img->cols, &img->rows, &color_stage);
+
+	Color **image = (Color **) malloc(sizeof(Color*) * img->rows);
+
+	for (int i = 0; i < img->rows; i ++) {
+		image[i] = (Color *)malloc(img->cols * sizeof(Color));
+		for (int j = 0; j < img->cols; j ++) {
+			fscanf(fp, "%hhu %hhu %hhu", &image[i][j].R, &image[i][j].G, &image[i][j].B);
+		}
+	}
+
+	img->image = image;
+
+	fclose(fp);
+
+	//process the input file
+	Image *new_image = steganography(img);
+
+	writeData(new_image);
+
+	freeImage(img);
+	freeImage(new_image);
+	return 0;
 }
