7d8dcc894bd4c214e1dbe1c60ec00610636e659a
adurey_laptop_lenove_yoga_14c <1721966349@qq.com>
2024-04-16 16:04:16 +0800

    lab02 E2 done

--- fa20-lab-starter/lab02/lfsr.c
+++ fa20-lab-starter/lab02/lfsr.c
@@ -4,7 +4,20 @@
 #include <string.h>
 #include "lfsr.h"
 
+// Return the nth bit of x.
+// Assume 0 <= n <= 15
+unsigned get_bit_uint16_t(uint16_t x,
+                 uint16_t n) {
+    // YOUR CODE HERE
+    // Returning -1 is a placeholder (it makes
+    // no sense, because get_bit only returns 
+    // 0 or 1)
+    return (x >> n) & 0b1;
+}
+
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t msb = get_bit_uint16_t(*reg, 0) ^ get_bit_uint16_t(*reg, 2) ^ get_bit_uint16_t(*reg, 3) ^ get_bit_uint16_t(*reg, 5);
+    (*reg) = ((*reg) >> 1) + (msb << 15);
 }
 
--- fa20-lab-starter/lab02/lfsr.h
+++ fa20-lab-starter/lab02/lfsr.h
@@ -1,1 +1,2 @@
+unsigned get_bit_uint16_t(uint16_t x, uint16_t n);
 void lfsr_calculate(uint16_t *reg);
